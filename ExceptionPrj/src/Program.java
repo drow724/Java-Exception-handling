c4e93603cf129962fe5545696c4655f7cb31e11b
Macintosh <drow724@naver.com>
2021-03-04 13:20:45 +0900

    예외 클래스 생성과 던지기(throw)

--- ExceptionPrj/src/Program.java
+++ ExceptionPrj/src/Program.java
@@ -1,15 +1,19 @@
 
 public class Program {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws 천을_넘는_예외, 음수가_되는_예외 {
 		Calculator calc = new Calculator();
 //		calc.add();
 // 		인스턴스화 시켜서 사용 Cal클래스 전역 변수를 사용
 		int result = 0;
-		result = Calculator.add(3,4);
+		result = Calculator.add(3,-4);
+		System.out.printf("add : %d\n", result);
 		result = Calculator.sub(3,4);
+		System.out.printf("sub : %d\n", result);
 		result = Calculator.multi(3,4);
+		System.out.printf("multi : %d\n", result);
 		result = Calculator.div(3,4);
+		System.out.printf("div : %d\n", result);
 //		스태틱 메소드 방식으로 사용
 //		넘겨받은 지역 변수를 사
 	}
---
+++ ExceptionPrj/src/천을_넘는_예외.java
@@ -0,0 +1,4 @@
+
+public class 천을_넘는_예외 extends Exception {
+
+}
